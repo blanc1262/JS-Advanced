95b7bab9c126334b59db0c813a3bdbe17f278919
blanc1262 <benedictlicong@gmail.com>
2025-03-18 19:06:31 +0800

    JS Advanced

--- JS1/main.js
+++ JS1/main.js
@@ -2,32 +2,34 @@ const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 const btn4 = document.getElementById("btn4");
-
+const btn5 = document.getElementById("btn5");
 const tbl = document.getElementById("tblNumbers");
 
-
 let total = 0;
-
 let numbersArr = new Array();
 
 function insertNumber() {
     const txtNumber = document.getElementById("txtNum").value;
-
     let num;
-    let regex = /^[0-9]+$/; // regular expression for checking valid positive number values.
-
+    let regex = /^[0-9]+$/;
 
     if(txtNumber.match(regex)){
         num = parseInt(txtNumber);
         numbersArr.push(num);
         console.log(numbersArr);
         document.getElementById("txtNum").value = "";
+
+        const sortValue = document.getElementById("sortOptions").value;
+        if (sortValue === "asc") {
+            numbersArr.sort((a, b) => a - b);
+        } else if (sortValue === "desc") {
+            numbersArr.sort((a, b) => b - a);
+        }
     } else {
         alert("Please input a positive number");
         document.getElementById("txtNum").value = "";
         return;
     }
-
     iterateNumbers();
 }
 
@@ -49,13 +51,11 @@ btn3.addEventListener("click", () => {
     numbersArr = [];
     total = 0;
 
-    // reset all trs
     while(tbl.hasChildNodes()) {
         tbl.removeChild(tbl.firstChild);
     }
 
     document.getElementById("btn4").style.display = "none";
-
 });
 
 btn4.addEventListener("click", () => {
@@ -64,13 +64,11 @@ btn4.addEventListener("click", () => {
     const tdTotalValue = document.createElement("td");
 
     trTotal.style.height = "30px";
-
     tdTotalLabel.style.fontWeight = "bold";
     tdTotalLabel.innerHTML = "TOTAL";
-
     tdTotalValue.style.textDecoration = "underline";
     tdTotalValue.innerHTML = total;
-        
+
     trTotal.appendChild(tdTotalLabel);
     trTotal.appendChild(tdTotalValue);
     tbl.appendChild(trTotal);
@@ -83,10 +81,9 @@ function deleteNumber(i) {
 }
 
 function editNumber(i) {
-
     const editTxt = prompt("Enter new number: ", numbersArr[i]);
-    const regex = /^[0-9]+$/; // regular expression for checking valid positive number values.
-    
+    const regex = /^[0-9]+$/;
+
     if(editTxt == null || editTxt == "") {
         alert("You did not input a new value!");
     } else {
@@ -101,25 +98,19 @@ function editNumber(i) {
 }
 
 function iterateNumbers() {
-    // reset all trs
     while(tbl.hasChildNodes()) {
         tbl.removeChild(tbl.firstChild);
     }
 
     if(!(numbersArr.length == 0)) {
-
         total = 0;
 
-        console.log(`Array Length: ${numbersArr.length}`);
-
-        // Loop for iterating numbers from the array in a table
-        for(let i=0 ; i < numbersArr.length ; i++) {
-
+        for(let i = 0; i < numbersArr.length; i++) {
             const tr = document.createElement("tr");
             const td1 = document.createElement("td");
             const td2 = document.createElement("td");
-            const td3 = document.createElement("td"); // for the delete button
-            const td4 = document.createElement("td"); // for the delete button
+            const td3 = document.createElement("td"); 
+            const td4 = document.createElement("td"); 
             const btnDelete = document.createElement("button");
             const btnEdit = document.createElement("button");
 
@@ -128,7 +119,7 @@ function iterateNumbers() {
 
             td2.style.width = "70px";
 
-            if(numbersArr[i] %2 == 0) {
+            if(numbersArr[i] % 2 == 0) {
                 td2.style.color = "green";
                 td2.innerHTML = "EVEN";
             } else {
@@ -151,16 +142,11 @@ function iterateNumbers() {
             tr.appendChild(td4);
 
             tbl.appendChild(tr);
-
-            if(!(numbersArr.length == 0)) {
-                document.getElementById("btn4").style.display = "inline";
-            }
-            
             total += numbersArr[i];
-            console.log(numbersArr[i]);
-
-            console.log(`Total: ${total}`)
         }
+
+        total = numbersArr.reduce((acc, num) => acc + num, 0);
+        document.getElementById("btn4").style.display = "inline";
     } else {
         total = 0;
         document.getElementById("btn4").style.display = "none";
@@ -168,3 +154,63 @@ function iterateNumbers() {
 
 }
 
+btn5.addEventListener("click", () => {
+    if (numbersArr.length === 0) {
+        alert("Please enter a number first!");
+        return;
+    }
+
+    let highest = numbersArr[0];
+    let lowest = numbersArr[0];
+
+    for (let i = 1; i < numbersArr.length; i++) {
+        if (numbersArr[i] > highest) {
+            highest = numbersArr[i];
+        }
+        if (numbersArr[i] < lowest) {
+            lowest = numbersArr[i];
+        }
+    }
+
+    const trHighest = document.createElement("tr");
+    const tdHighestLabel = document.createElement("td");
+    const tdHighestValue = document.createElement("td");
+
+    trHighest.style.height = "30px";
+    tdHighestLabel.style.fontWeight = "bold";
+    tdHighestLabel.innerHTML = "HIGHEST";
+    tdHighestValue.style.textDecoration = "underline";
+    tdHighestValue.style.color = "red";
+    tdHighestValue.innerHTML = highest;
+
+    trHighest.appendChild(tdHighestLabel);
+    trHighest.appendChild(tdHighestValue);
+    tbl.appendChild(trHighest);
+
+    const trLowest = document.createElement("tr");
+    const tdLowestLabel = document.createElement("td"); 
+    const tdLowestValue = document.createElement("td");
+
+    trLowest.style.height = "30px";
+    tdLowestLabel.style.fontWeight = "bold";
+    tdLowestLabel.innerHTML = "LOWEST";
+    tdLowestValue.style.textDecoration = "underline";
+    tdLowestValue.style.color = "orange";
+    tdLowestValue.innerHTML = lowest;
+
+    trLowest.appendChild(tdLowestLabel);
+    trLowest.appendChild(tdLowestValue);
+    tbl.appendChild(trLowest);
+});
+
+document.getElementById("sortOptions").addEventListener("change", function() {
+    const sortValue = this.value;
+
+    if (sortValue === "asc") {
+        numbersArr.sort((a, b) => a - b);
+    } else if (sortValue === "desc") {
+        numbersArr.sort((a, b) => b - a);
+    }
+
+    iterateNumbers();
+});
